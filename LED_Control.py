2fb9c92ae4bb917a4d871680afcf30481189bcbc
akrapovic-de <55234745+akrapovic-de@users.noreply.github.com>
2022-02-24 10:58:57 +0100

    Add files via upload

---
+++ LED_Control.py
@@ -0,0 +1,22 @@
+# Simple test for NeoPixels on Raspberry Pi
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+import neopixel
+
+# The number of NeoPixels
+num_pixels = 144
+# Pin the LED_Strip is connected
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pixel_pin = GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
+
+status_door = input("ROS::status door (open)?")
+
+if status_door == "open":
+	pixels[0:num_pixels] = (255, 0, 0) #rot
+else:
+	pixels[0:num_pixels] = (0, 255, 0) #green
